39af4a8ec16eca10138989b19e6586058712d8c4
Maarij_Aqeel <92783006+Maarij-Aqeel@users.noreply.github.com>
2024-09-12 15:02:30 +0500

    Fixed Game_quit in main_menu

--- test.py
+++ test.py
@@ -18,45 +18,44 @@ def main():
 
     # Create a Game instance
     game = Game(level, score, highscore)
-    game.main_menu()
-
-    while not game_won:
-        result = game.run()
-
-        # Update highscore
-        if game.score > highscore:
-            highscore = game.score
-            f = open('highscore.txt', 'w')
-            f.write(str(highscore))
-            f.close()
-
-        # Check if game is won
-        if result == 'level_complete':
-            level += 1
-            if level == 4:
-                game_won = True
-            elif level == 3:
-                game.lives = 2
-            else:
-                game.lives = 3
-            # Check win
-            if game_won:
-                play_again = game.display_you_won()
-                if play_again == "play_again":
+    if game.main_menu():
+        while not game_won:
+            result = game.run()
+    
+            # Update highscore
+            if game.score > highscore:
+                highscore = game.score
+                f = open('highscore.txt', 'w')
+                f.write(str(highscore))
+                f.close()
+    
+            # Check if game is won
+            if result == 'level_complete':
+                level += 1
+                if level == 4:
+                    game_won = True
+                elif level == 3:
+                    game.lives = 2
+                else:
+                    game.lives = 3
+                # Check win
+                if game_won:
+                    play_again = game.display_you_won()
+                    if play_again == "play_again":
+                        level = 1
+                        score = 0
+                        game = Game(level, score)
+                        game_won = False
+    
+    
+            elif result == "restart":
+                    # Reset the game
                     level = 1
                     score = 0
                     game = Game(level, score)
-                    game_won = False
-
-
-        elif result == "restart":
-                # Reset the game
-                level = 1
-                score = 0
-                game = Game(level, score)
-        elif result == "exit":
-            pygame.quit()
-            sys.exit()
+            elif result == "exit":
+                pygame.quit()
+                sys.exit()
     
 if __name__ == "__main__":
     main()
